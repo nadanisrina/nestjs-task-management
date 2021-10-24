96296f2fdd6110cf23383512909809c293ad5832
nadans <nisrinaseptiana1@gmail.com>
2021-10-24 16:06:27 +0700

    update service tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
-@Controller('/tasks')
+@Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {
-    this.tasksService = tasksService;
+    // this.tasksService = tasksService;
   } // service injected and become class property
 
-  // @Get()
-  // async getAllTasks() {
-  //   return await this.tasksService.getAllTasks();
-  // }
+  @Get()
+  getAllTasks() {
+    console.log('tasks controller');
+    return this.tasksService.getAllTasks();
+  }
 }
