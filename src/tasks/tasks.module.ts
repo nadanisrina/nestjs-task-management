c6014fa79e56df1d128112cd308a527fd58db4f6
nadans <nisrinaseptiana1@gmail.com>
2021-10-24 14:38:58 +0700

    make tasks module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
 
-@Module({})
+@Module({
+  controllers: [TasksController],
+  providers: [TasksService],
+  // service injected to controller
+})
 export class TasksModule {}
